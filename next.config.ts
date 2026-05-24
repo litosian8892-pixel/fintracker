63c42b9523bba6335bd96a7ef1742dbdb704d7f5
litos <litosian8892@gmail.com>
2026-05-24 11:23:38 +0700

    fix: force webpack build agar Service Worker PWA berhasil di-generate

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,12 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  // Baris swcMinify sudah kita hapus karena Next.js 16 sudah otomatis melakukannya
-  disable: process.env.NODE_ENV === "development", // PWA hanya aktif di Production (Vercel)
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // Mencegah error Turbopack Next.js 16
 };
 
 export default withPWA(nextConfig);
